c625f159d427c7c767363241a35c2a88d10cc638
Shivani Malik <malikshivani04789@gmail.com>
2026-06-24 22:49:03 +0530

    first phython code

---
+++ first.py
@@ -0,0 +1,86 @@
+# # def greet():
+# #     print("hello,students")
+
+
+# # greet()
+
+
+
+# # def your_name(name):
+# #     print(name)
+
+
+# # your_name("shivani")
+
+# print(5%2)
+
+
+# def check_even_odd(num):
+#     if num % 2 == 0:
+#         print("even number")
+#     else:
+#         print("Odd number")
+
+
+
+# check_even_odd(5)
+
+# string operations
+# indexng
+# name="shivani"
+# print(name[0])
+
+# print(name[1])
+# print(name[-1])
+# 
+# print(name[-4])
+
+# length
+# name="shivam"
+# print(len(name))
+
+# name="buggi"
+# print(len(name))
+
+# concatenation (string jodna)
+# a="shivani"
+# b="hunny"
+# print(a," ",b)
+# print(a+" "+b)
+
+# repetition 
+# print("Hi " * 3)
+
+# upper case
+# name="anshu"
+# print(name.upper())
+
+# lower case
+# name="chahat"
+# print(name.lower())
+
+# check kre  ki word h ya nahi
+# text="I love phython"
+# print("india" in text)
+
+# slicing
+
+# name="hunny"
+# print(name[0:3])
+# print(name[1:5])
+
+# print(name[3:])
+# print(name[-1])
+# print(name[-1:-4:-1])
+
+# step vali slicing
+# name="shivani"
+# print(name[-3])
+
+
+
+
+
+
+
+
