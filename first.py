01c97b56905e0d2c73221208097c25b7dfb03488
Shivani Malik <malikshivani04789@gmail.com>
2026-06-25 18:37:04 +0530

    practice code update

--- first.py
+++ first.py
@@ -73,9 +73,110 @@
 # print(name[-1])
 # print(name[-1:-4:-1])
 
-# step vali slicing
+# FUNCTION
+#1 def greet():
+    # print("hello,student")
+
+# greet()
+
+#2 def my_name(name):
+    # print("shivani")
+
+# my_name("name")
+
+#3 def add_numbers(a,b):
+    # print(a+b)
+
+# add_numbers(6,5)
+
+
+#4 def check_even_odd(num):
+    # if num % 4==0 or 4==1:
+    #  print("even number")
+    # else:
+    #  print("odd number")
+# check_even_odd(6)
+
+
+#5 def square(num):
+#    print(num * num)
+
+# square(5)
+
+
+# num=(1,2,3,4,5,6,7,)
+# print(num[0])
+# print(num[5])
+# print(num[-3])
+
+
+# name="Hello, world"
+# print(name[-1])
+
+# print(name[5])
+
+
+# # Slicing
+
 # name="shivani"
-# print(name[-3])
+# print(name[:])
+# print(name[::])
+
+# print(name[:4])
+# print(name[3:])
+
+# print(name[::-1])
+# print(name[-2:])
+
+# print(name[1:4:2])
+# print(name[1:8])
+
+# print(name[-1:])
+# print(name[:-1])
+
+# # pracice questions
+
+# s="programming"
+# print(s[0:5])
+
+# nums="10,20,30,40,50,60"
+# print(nums[0:8])
+
+# name="python"
+# print(name[::-1])
+
+# text="Artificial Intelligence"
+# print(text[0:10])
+
+# colors=["red","blue","green","yellow","black"]
+# print(colors[-2:])
+
+# numbers=[1,2,3,4,5,6,7,8,9,10]
+# print(numbers[::2])
+# print(numbers[0:10:2])
+
+
+# s="Datascience"
+# print(s[1:10:2])
+# 
+# marks=[45,67,89,90,34,56,78]
+# print(marks[1:-1])
+
+# sentence="Machine Learning with Python"
+# print(sentence[8:16])
+
+# nums=[100,200,300,400,500,600]
+# nums[1:5] = nums[1:5] [::-1]
+# print(nums) 
+
+data=[[1,2,3],[4,5,6],[7,8,9]]
+
+
+
+
+
+
+
 
 
 
