47cbcbb9c2422f0175307d06fe20fa72372971c5
Shivani Malik <malikshivani04789@gmail.com>
2026-06-26 12:42:31 +0530

    Added question.py

---
+++ question.py
@@ -0,0 +1,75 @@
+# name=input("enter your name")
+# age=int(input("enter your age"))
+# print("Hello", name,)
+# print("I am",age,"years old")
+
+# a=int(input("first number"))
+# b=int(input("second number"))
+# sum=a+b
+# print(sum)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+
+
+# radius = float(input("enter radius"))
+# area=3.14 *  radius * radius
+# print("area of circle=",area)
+
+# num=4
+# if num % 3 == 0:
+#     print("even number")
+# else:
+#     print("odd number")
+
+# a=4
+# b=3
+# c=6
+# if a > b:
+#     print("larger")
+# elif b < c:
+#     print("smaller")
+# else:
+#     print("equal")
+
+# marks=70
+# if marks >= 90:
+#     print("Grade A")
+# elif marks >=80:
+#     print("Grade B")
+# elif marks >= 70:
+#     print("Grde C")
+# else:
+#     print("Grade D")
+
+# year=2024
+# if year % 4==0:
+#     print("leap year")
+# else:
+#     print("not leap year")
+
+# num=1
+# while num <=100:
+#     print(num)
+#     num += 1
+
+# num=2
+# while num <=20:
+#     print(num)
+#     num += 2
+
+# num=10
+# while num >=1:
+#     print(num)
+#     num -=1
+
+# name=(input("enter a string"))
+# if name==name [::-1]:
+#     print("Palindrome")
+# else:
+#     print("Not Palindrome")
+
+# sentence=input("enter a sentence")
+# print(len(sentence))
+
+
