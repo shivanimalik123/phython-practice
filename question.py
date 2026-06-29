6745d55630348d8aa18b6166c5d5e78387f3eb58
Shivani Malik <malikshivani04789@gmail.com>
2026-06-29 21:21:35 +0530

    Updated files

--- question.py
+++ question.py
@@ -73,3 +73,143 @@
 # print(len(sentence))
 
 
+# name=input("enter your name")
+# age=int(input("enter your name"))
+# print("hello",name,"i am",age ,"years old")
+
+# radius=float(input("enter a radius"))
+# area= 3.14 *radius * radius
+# print("area of circle" ,area)
+
+# num=12
+# if num % 4==0:
+    # print("even number")
+# else:
+    # print("odd number")
+
+# a=4
+# b=3
+# a,b=b,a
+# print("a",a)
+# print("b",b)
+
+# celcius=float(input("enter temperature in celcius"))
+# fahrenheit=(celcius * 9/5)+32
+# print(fahrenheit)
+
+# radius=float(input("enter area"))
+# area= 3.14 * radius *radius
+# print("area of circle",area)
+
+# num=6
+# if num % 4:
+#     print("even number")
+# else:
+#     print("odd number")
+
+# year=int(input("enter year"))
+# if year % 400==0:
+#     print("leap year")
+# else:
+#     print("not leap year")
+
+
+# for i in range(1,101):
+    # print(i)
+
+# i=1
+# while i <=10:
+    # print("5 x",i,"=", 5 * i)
+    # i +=1
+
+# for i in range(1,11):
+    # print(i * 5)
+
+# num=10
+# while num >=1:
+#     print(num)
+#     num-=1
+
+# def greet():
+#     print("hello ,students")
+# greet()
+
+# def my_name(name):
+#     print("shivani")
+# my_name("name")
+
+# def add_numbers(a,b):
+#     print(a+b)
+# add_numbers(3,5)
+
+# def check_even_odd(num):
+#     if num % 2 == 0:
+#         print("even")
+#     else:
+#         print("odd")
+# check_even_odd(5)
+
+# def square(num):
+#     print(num * num)
+# square(4)
+
+# def calSum(a):
+#     global x
+#     x=10
+#     sum=a+x
+#     return sum
+# print(calSum(3))
+# print(x)
+
+# def mulOfTwo(a):
+#     global x
+#     x=4
+#     mul=a * x
+#     return mul
+# print(mulOfTwo(2))
+# print(x)
+
+
+# lambda perameters:expression
+# add=lambda a,b : a+b
+# print(add(3,4))
+
+# global and local and lambda practice questions
+
+# square=lambda a : a*a
+# print(square(4)) 
+
+# add=lambda a,b : a+b
+# print(add(5,6))
+
+# cube=lambda a : a**3
+# print(cube(2))
+
+# mul=lambda a,b: a*b
+# print(mul(2,3))
+
+
+# x=5
+
+# def show():
+#     y=4
+#     print(y)
+# show()
+#  print(x)
+
+
+# name="Rahul"
+
+# def show():
+#     print(name)
+
+# show()
+
+# x=10
+# def show():
+#     x=20
+#     print(x)
+
+# show()
+# print(x)
+    
