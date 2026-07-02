52ddcd02d80fd4e2b6f157401e466acc2ac6a22a
Shivani Malik <malikshivani04789@gmail.com>
2026-07-02 19:14:41 +0530

    updated code

---
+++ app.py
@@ -0,0 +1,122 @@
+# import os
+# f = open("student.txt","w")
+# f.write("Name:John")
+# f.write("Course:Python")
+# f.close()
+
+
+# f=open("student.txt","r")
+# print(f.read())
+# f.close()
+
+
+# f=open("student.txt","a")
+# f.write("/nCity:delhi")
+# f.close()
+
+# f=open("malik.txt", "r")
+# content=f.read()
+# print(content)
+# f.close()
+
+
+# f=open("malik.txt","w")
+# f.write("hii suman")
+# f.close()
+
+# f=open("malik.txt","a")
+# f.write("hlo shivani")
+# f.close()
+
+# f=open("malik.txt","r+")
+# print(f.read())
+# f.write("\nshe is my sister")
+# f.close()
+# f=open("malik.txt","r")
+# print(f.read())
+# f.close()
+
+# f=open("malik.txt","w")
+# f.close()
+
+# f=open("malik.txt","w+")
+# f.write("Good Night")
+# f.seek(0)
+# print(f.read())
+# f.close()
+
+# f=open("malik.txt","a+")
+# f.write("\nHii Gappi")
+# f.seek(0)
+# print(f.read())
+# f.close()
+
+
+# def table(num):
+#     for i in range(1,11):
+#         print(num)
+
+# table(3)
+
+# add=lambda a,b:a+b
+# print(add(3,5))
+
+# square=lambda a:a*a  
+# print(square(4))
+
+# f=open("notes.txt","w")
+# f.close()
+
+# f=open("student.txt","w")
+# f.write(" Name:John")
+# f.write(" Course:Python")
+# f.close()
+
+# f=open("student.txt","r")
+# print(f.read())
+
+# f=open("student.txt","a")
+# f.write("\nCity:Delhi")
+# f.close()
+
+# f=open("notes.txt","w")
+# f.write("Python File Handling")
+# f.close()
+
+# f=open("student.txt","r")
+# print(f.read())
+# f.close()
+
+# x=3
+# def add(a):
+#     y=3
+#     print(a)
+# add(4)
+# print(x)
+    
+
+# def add(a):
+#     global x
+#     x=3
+#     print(a)
+# add(5)
+# print(x)
+
+
+# x=11
+
+# def test():
+#     print(x)
+
+# test()
+# print(x)
+
+
+# name="shivani"
+# def greet():
+#     print("Hello",name)
+# def bye():
+#     print("Byy",name)
+
+# greet()
+# bye()
