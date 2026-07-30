c87e13f4c86a987bb0f3c588d6e094c99d40a2a0
Shivani Malik <malikshivani04789@gmail.com>
2026-07-30 12:32:37 +0530

    Updated new files

---
+++ Basic_Calculator/numpy_practice.py
@@ -0,0 +1,65 @@
+# import numpy as np
+# print("NumPy Imported Successfully")
+
+# import numpy as np
+# a = np.array([1,2,3,4,5])
+# print(a)
+
+# import numpy as np
+# a =np.array ([1,2,3,4,5])
+# print(type(a))
+
+# import numpy as np
+# marks = np.array([75,80,65,90,85])
+# print(marks)
+
+# import numpy as np
+# a = np.array([1,2,3,4])
+# print(a.dtype)
+
+# import numpy as np
+# arr = np.array([10,20,30,40,50])
+# print(arr[0])
+
+# import numpy as np
+# a = np.array([10,20,30,40,50])
+# print(a[2])
+
+# import numpy as np
+# arr = np.array([1,2,3,4,5])
+# print(arr[-1])
+
+# import numpy as np
+# arr = np.array([1,2,3,4,5])
+# print(arr[0:3])
+
+# import numpy as np
+# arr = np.array([1,2,3,4,5])
+# print(arr[1:4])
+
+# import numpy as np
+# arr = np.array((1,2,3,4,5))
+# print(arr)
+
+# import numpy as np
+# arr = np.array([1,2,3,4,5])
+# arr[2] = 100
+# print(arr)
+
+# import numpy as np
+# arr = np.array([1,2,3,4,5])
+# print(arr.ndim)
+
+# import numpy as np
+# arr = np.array([[1,2,3,4],
+#                 [5,6,7,8]])
+# print(arr.shape)
+
+
+# import numpy as np
+# arr = np.array([1,2,3,4,3,5])
+# print(arr.size)
+
+import numpy as np 
+a=([[1,2,3,4],[2,3,4,5]])
+print(a)
