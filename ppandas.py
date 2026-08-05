43aeca0942cc0d4ed3c237abaa182aecb2732525
Shivani Malik <malikshivani04789@gmail.com>
2026-08-05 12:19:07 +0530

    Updated new files

--- ppandas.py
+++ ppandas.py
@@ -1,1 +1,53 @@
+# import pandas as pd
+# data = [100,200,300]
+# series = pd.Series(data)
+# print(series)
+
+# import pandas as pd
+# data = [100,200,300]
+# series = pd.Series(data,index=["a","b","c"])
+# print(series)
+
+# import pandas as pd
+# data = [100,200,300]
+# series = pd.Series(data,index=["a","b","c"])
+# print(series.to_string(index=False))
+
+# import pandas as pd
+# data = [100,200,300]
+# series = pd.Series(data)
+# print(series.to_string(index=False))
+
+# import pandas as pd
+# data = [5,10,15,20]
+# series = pd.Series(data)
+# print(series)
+
+# import pandas as pd 
+# data = (1,2,3,4)
+# series = pd.Series(data)
+# print(series.iloc[1])
+
+# import pandas as pd
+# data = {
+#     "A":10,
+#     "B":20,
+#     "C":30
+# } 
+# series = pd.Series(data)
+# print(series.loc["B"])
+
+# import pandas as pd
+# data = {
+#     "A":10,
+#     "B":20,
+#       "C":30
+# }
+# series = pd.Series(data)
+# series.loc["C"]=35
+# print(series)
+
 import pandas as pd
+data =[5,10,15,20]
+series = pd.Series(data)
+print(series[series>=20])
