c79c62470dfdebc6a2a911cea3ffc1d23a0fbca8
Shivani Malik <malikshivani04789@gmail.com>
2026-08-09 17:29:40 +0530

    Updated ne files

--- Basic_Calculator/numpy_matplotlib.py
+++ Basic_Calculator/numpy_matplotlib.py
@@ -1,111 +1,12 @@
-# 1.
-# import numpy as np
-# arr=np.arange(1,11)
-# print(arr)
-
-# 2.
-# import numpy as np
-# arr=np.arange(2,21,2)
-# print(arr)
-
-# 3.
-# import numpy as np
-# a=np.array([1,2,3,4,5])
-# print(a)
-
-# 4.
-# import numpy as np
-# b = np.array([1,2,3,45])
-# print(b.dtype)
-
-# 5.
-# import numpy as np
-# arr = np.array([3,6,9,12,15])
-# print(arr.size)
-
-# 6.
-# import numpy as np
-# arr = np.array([[2,4,6,8],
-#                 [3,6,9,12]])
-# print(arr)
-
-# 7.
-# import numpy as np
-# arr = np.zeros(5)
-# print(arr)
-
-# 8.
-# import numpy as np
-# arr = np.array([[1,2,3],[4,5,6]])
-# print(arr.shape)
-
-# 9.
-# import numpy as np
-# arr = np.arange(1,13)
-# arr = arr.reshape(3,4)
-# print(arr)
-
-# 10.
-# import numpy as np
-# arr = np.arange(1,13)
-# arr = arr.reshape(2,6)
-# print(arr)
-
-# 11.
-# import numpy as np
-# arr = np.array([1,2,3,4,5,6,7,8])
-# arr = arr.reshape(2,4)
-# print(arr)
-
-# 12.
-# import numpy as np
-# arr = np.arange(1,9)
-# arr = arr.reshape(4,2)
-# print(arr)
-
-# JOIN & SPLIT
 
 # 1.
-# import numpy as np
-# arr1 = np.array([1,2,3])
-# arr2 = np.array([4,5,6])
-# arr = np.concatenate((arr1,arr2))
-# print(arr)
-
-# 2.
-# import numpy as np
-# arr1 = np.array([[1,2],[3,4]])
-# arr2 = np.array([[5,6],[7,8]])
-# arr = np.vstack((arr1,arr2))
-# print(arr)
-
-# 3.
-# import numpy as np
-# arr1 = np.array([[1,2],[3,4]])
-# arr2 = np.array([[5,6],[7,8]])
-# arr = np.hstack((arr1,arr2))
-# print(arr)
-
-# 4.
-# import numpy as np
-# arr = np.array([1,2,3,4,5,6])
-# new = np.split(arr,3)
-# print(new)
-
-# 5.
-# import numpy as np
-# arr = np.array([1,2,3,4,5,6,7,8])
-# old = np.split(arr,4)
-# print(old)
-
-# 6.
 # import matplotlib.pyplot as plt
 # x = [1,2,3,4,5]
 # y = [2,4,6,8,10]
 # plt.plot(x , y)
 # plt.show() 
 
-# 7.
+# 2.
 # import matplotlib.pyplot as plt
 # x = [1,2,3,4,5]
 # y = [2,4,6,8,10]
@@ -115,12 +16,12 @@
 # plt.ylabel("Y-axis")
 # plt.show()
 
-# 8.
+# 3.
 # import matplotlib.pyplot as plt
 # plt.plot([1,2,3,4,5],[2,4,6,8,10])
 # plt.show()
 
-# 9.
+# 4.LABELS & TITLES
 # import matplotlib.pyplot as plt
 # x = [1,2,3,4,5]
 # y = [2,4,6,8,10]
@@ -130,7 +31,7 @@
 # plt.title("Simple Line Plot")
 # plt.show()
 
-# 10.
+# 5.GRID PLOT
 # import matplotlib.pyplot as plt
 # x = [1,2,3,4,5]
 # y = [2,4,6,8,10]
@@ -139,7 +40,7 @@
 # plt.grid(True)
 # plt.show()
 
-# 11.
+# 6. LEGEND
 # import matplotlib.pyplot as plt
 # x = [1,2,3,4,5]
 # y1 = [2,4,6,8,10]
@@ -150,7 +51,7 @@
 # plt.legend()
 # plt.show()
 
-# 12.
+# 7. BAR CHARTS VERTICAL
 # import matplotlib.pyplot as plt
 # categories = ["A","B","C","D"]
 # values = [4,7,2,9]
@@ -160,7 +61,40 @@
 # plt.ylabel("Values")
 # plt.show()
 
-
-
+# 8. PIE CHARTS
 # import matplotlib.pyplot as plt
+# sizes = [30,20,25,25]
+# labels = ["A","B","C","D"]
+# plt.pie(sizes,labels=labels,autopct="%1.1f%%")
+# plt.title("Pie Chart Example")
+# plt.show()
 
+# 9.BAR CHARTS (HORIZONTAL)
+# import matplotlib.pyplot as plt
+# categories = ["A","B","C","D"]
+# values = [2,4,6,8]
+# plt.barh(categories,values)
+# plt.title("Horizontal Bar Chart")
+# plt.xlabel("Values")
+# plt.ylabel("Categories")
+# plt.show()
+
+# 10.PIE CHART
+# import matplotlib.pyplot as plt
+# sizes = [10,20,30,40]
+# labels = ["A","B","C","D"]
+# plt.pie(sizes,labels=labels,autopct="%1.1f%%")
+# plt.title("Pie Chart Example")
+# plt.show()
+
+# 11.PLOT CUSTOMIZATION
+import matplotlib.pyplot as plt
+x = [1,2,3,4,5]
+y = [2,4,6,8,10]
+plt.plot(x,y,color="green",marker="o",linestyle="--",label="Line 1")
+plt.title("Customized Line Plot")
+plt.xlabel("X Axis")
+plt.ylabel("Y Axis")
+plt.grid(True)
+plt.legend()
+plt.show()
