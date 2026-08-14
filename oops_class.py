27a21671ab1c168a69a747e28d44f18d8dbadf5e
Shivani Malik <malikshivani04789@gmail.com>
2026-08-14 10:19:14 +0530

    Updated new files

---
+++ oops_class.py
@@ -0,0 +1,81 @@
+# class Person:
+#     def __init__(self, name, age=18):
+#         self.name=name
+#         self.age=age
+# p1 = Person("Email")
+# print(p1.age)
+
+# class Student:
+#     pass
+
+# class Student:
+#     pass
+# s1 = Student()
+
+# class Car:
+#     pass
+# car1 = Car()
+# car2 = Car()
+# car3 = Car()
+
+# class Student:
+#     pass
+# s1 = Student()
+# s1.name = "Shivani"
+# print(s1.name)
+
+# class Student:
+#     pass
+# s1 = Student()
+# s2 = Student()
+# s1.name="Ritesh"
+# s2.name="hunny"
+# print(s1.name)
+# print(s2.name)
+
+# class Student:
+#     pass
+# s1 = Student()
+# s1.name="Ram"
+# s1.age=18
+# s1.marks=78
+# print(s1.name)
+# print(s1.age)
+# print(s1.marks)
+
+# class Mobile:
+#     pass
+# m1 = Mobile()
+# m2 = Mobile()
+# m1.brand="Samsung"
+# m2.brand="Redmi"
+# print(m1.brand)
+# print(m2.brand)
+
+# class Book:
+#     pass
+# b1 = Book()
+# b1.title="Python Programming"
+# b1.price="458"
+# print(b1.title)
+# print(b1.price)
+
+# class Student:
+#     def display(self):
+#         print("Welcome to Python OOP")
+# s1 = Student()
+# s1.display()
+
+class Employee:
+    def display(self):
+        print("Name:",self.name)
+        print("Salary:",self.salary)
+e1 = Employee()
+e1.name="Shivani"
+e1.salary=100000
+e2 = Employee()
+e2.name="Anshu"
+e2.salary=58000
+e1.display()
+e2.display()
+
